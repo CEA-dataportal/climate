6644b1086ee6a76c5b4c175cb817ad3ffb1fc823
Vincent Turmine <vincent.turmine@gmail.com>
2023-11-29 10:40:01 +0100

    update

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,9 @@
 
-// Get the modal -Bangladesh Tangail
-var modal_t = document.getElementById("myModal_tangail");
+// Get the modal 
+var modal_t = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn_tangail");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("btn-close")[0];
@@ -24,30 +24,3 @@ window.onclick = function(event) {
     modal_t.style.display = "none";
   }
 }
-
-// Get the modal -Bangladesh Satkhira
-var modal_s = document.getElementById("myModal_satkhira");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn_satkhira");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("btn-close")[1];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal_s.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {  
-  modal_s.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal_s) {
-    modal_s.style.display = "none";
-  }
-}
-
